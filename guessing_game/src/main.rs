796d5ae8db4a05ae0e51fcb9e2a523c18a77e6fe
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-08-24 19:03:57 +0900

    add loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,25 +11,27 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // shadowing
-    let guess: u32 = guess
-        .trim() // trim \n
-        .parse()
-        .expect("Please type a number!");
+        // shadowing
+        let guess: u32 = guess
+            .trim() // trim \n
+            .parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
